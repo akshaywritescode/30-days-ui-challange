6986bf76feb491ce52d82fd92236cdac80c1710e
akshaywritescode <teamakshayyadav@gmail.com>
2025-07-13 12:44:26 +0530

    added break on list of projects

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="p-3">
       <h1 className="text-4xl font-bold mb-4">30 Days UI Challage</h1>
-      <div>
+      <div className="flex flex-col gap-3">
         <Link href={"/day-1"} className="underline">Day 1 - Google Location Overview Carousel Slider</Link>
         <Link href={"/day-2"} className="underline">Day 2 - Freelancer Card UI</Link>
       </div>
